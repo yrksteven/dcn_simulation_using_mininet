85e1bd23c80d60b7a5017f5b9f674a9a538d138d
yrksteven <yirksteven@gmail.com>
2016-07-27 20:53:38 +0800

    Code

---
+++ stpSetup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo ovs-vsctl set bridge s17 stp-enable=true
+sudo ovs-vsctl set bridge s18 stp-enable=true
+sudo ovs-vsctl set bridge s19 stp-enable=true
+sudo ovs-vsctl set bridge s20 stp-enable=true
+
+sudo ovs-vsctl set bridge s9 stp-enable=true
+sudo ovs-vsctl set bridge s10 stp-enable=true
+sudo ovs-vsctl set bridge s11 stp-enable=true
+sudo ovs-vsctl set bridge s12 stp-enable=true
+sudo ovs-vsctl set bridge s13 stp-enable=true
+sudo ovs-vsctl set bridge s14 stp-enable=true
+sudo ovs-vsctl set bridge s15 stp-enable=true
+sudo ovs-vsctl set bridge s16 stp-enable=true
+
+sudo ovs-vsctl set bridge s1 stp-enable=true
+sudo ovs-vsctl set bridge s2 stp-enable=true
+sudo ovs-vsctl set bridge s3 stp-enable=true
+sudo ovs-vsctl set bridge s4 stp-enable=true
+sudo ovs-vsctl set bridge s5 stp-enable=true
+sudo ovs-vsctl set bridge s6 stp-enable=true
+sudo ovs-vsctl set bridge s7 stp-enable=true
+sudo ovs-vsctl set bridge s8 stp-enable=true
